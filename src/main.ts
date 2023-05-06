95027b4aa34fb091f5777111b744a25b8dbe05bf
lauramura10 <62648418+lauramura10@users.noreply.github.com>
2023-05-06 13:34:02 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,56 @@
-import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
   template: `
-    <h1>Hello from</h1>
-    <a target="_blank" >
-      Learn more about Angular 
-    </a>
+  <button
+    type="button" [disabled]="canClick" (click)="sayMessage()"> Alert message
+  </button>
+  <h1>{{message2}}</h1>
+  <p
+    [id]="sayHelloId" [style.color]="fontColor"> You can set my color in the component!
+  </p>
+ 
+  <p>My color is {{ fontColor }}</p>
+  
+  <h2>Hello World: ngIf!</h2>
+ 
+  <button type="button" (click)="onEditClick()">Make text editable!</button>
+
+  <div class="luce" *ngIf="lucisingole.stato == 'ON'; else luceoff">
+    <h5> ON </h5>
+  </div>
+  <ng-template #luceoff>
+      <div class="luce">
+      <h5> OFF </h5>
+      </div>
+  </ng-template>
   `,
 })
-export class App {
-  name = 'Angular';
-}
+export class HelloWorldBindingsComponent {
+  fontColor = 'blue';
+  sayHelloId = 1;
+  canClick = false;
+  message = 'Hello, World';
+  message3 = "I'm read only!";
+  canEdit = true;
 
-bootstrapApplication(App);
+  sayMessage() {
+    alert(this.message);
+  }
+  message2: string = 'Angular';
+
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message3 = 'You can edit me!';
+    } else {
+      this.message3 = "I'm read only!";
+    }
+  }
+}
+bootstrapApplication(HelloWorldBindingsComponent);
