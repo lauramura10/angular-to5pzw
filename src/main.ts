3a232db2fe2026d7f2a947a4a4af79b828f0d862
lauramura10 <62648418+lauramura10@users.noreply.github.com>
2023-05-06 13:36:26 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -7,50 +7,17 @@ import { NgModule } from '@angular/core';
   selector: 'my-app',
   standalone: true,
   template: `
-  <button
-    type="button" [disabled]="canClick" (click)="sayMessage()"> Alert message
-  </button>
-  <h1>{{message2}}</h1>
-  <p
-    [id]="sayHelloId" [style.color]="fontColor"> You can set my color in the component!
-  </p>
+  <h1>Test</h1>
  
   <p>My color is {{ fontColor }}</p>
-  
-  <h2>Hello World: ngIf!</h2>
- 
-  <button type="button" (click)="onEditClick()">Make text editable!</button>
-
-  <div class="luce" *ngIf="lucisingole.stato == 'ON'; else luceoff">
-    <h5> ON </h5>
-  </div>
-  <ng-template #luceoff>
-      <div class="luce">
-      <h5> OFF </h5>
-      </div>
-  </ng-template>
   `,
 })
 export class HelloWorldBindingsComponent {
   fontColor = 'blue';
-  sayHelloId = 1;
-  canClick = false;
   message = 'Hello, World';
-  message3 = "I'm read only!";
-  canEdit = true;
 
   sayMessage() {
     alert(this.message);
-  }
-  message2: string = 'Angular';
-
-  onEditClick() {
-    this.canEdit = !this.canEdit;
-    if (this.canEdit) {
-      this.message3 = 'You can edit me!';
-    } else {
-      this.message3 = "I'm read only!";
-    }
   }
 }
 bootstrapApplication(HelloWorldBindingsComponent);
